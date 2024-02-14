5fc932177ff70c57fc7692e77ff300d1b1057bb5
shaikh-akbar-96 <akbarhshaikh1996@gmail.com>
2024-02-14 17:13:43 +0530

    project completed

--- index.js
+++ index.js
@@ -29,3 +29,33 @@ submitFormEle.addEventListener("submit", (e) => {
   liEle.appendChild(deleteItem);
   ulEle.appendChild(liEle);
 });
+
+let groceryList = document.getElementById("grocery-list");
+let check = document.getElementById("hide");
+
+check.addEventListener("change", () => {
+  if (check.checked) {
+    groceryList.style.display = "none";
+  } else {
+    groceryList.style.display = "block";
+  }
+});
+
+let searchGroceryItems = document.getElementById("search-item");
+let searchInput = document.getElementById("input-search");
+console.log(searchGroceryItems);
+console.log(searchInput.value);
+searchGroceryItems.addEventListener("keyup", (e) => {
+  let searchTerm = e.target.value.toLowerCase();
+  let groceryList = document.getElementsByTagName("ul");
+  const grocery = document.getElementsByTagName("li");
+  console.log(grocery);
+  for (let i = 0; i < grocery.length; i++) {
+    let groceryName = grocery[i].firstElementChild.textContent.toLowerCase();
+    if (groceryName.indexOf(searchTerm) !== -1) {
+      grocery[i].style.display = "flex";
+    } else {
+      grocery[i].style.display = "none";
+    }
+  }
+});
